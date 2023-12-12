1fd1cc1d81d1cd131a4a52bc2b4495579cdf8a80
EzequielRindello <rindelloezequiel@gmail.com>
2023-12-12 19:19:38 -0300

    menu de hamburguesa implementado

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ currentDate.textContent = formattedDate;
 const lastModifiedDate = new Date(document.lastModified).toLocaleDateString("en-US", options);
 document.getElementById("lastModified-placeholder").textContent = lastModifiedDate;
 
+// movile menu de hamburguesa
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.querySelector(".navbar");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+        // cada vez que pasa un addEventListener() cambia el i
+        menuIcon.querySelector("i").classList.toggle("bi-list");
+        menuIcon.querySelector("i").classList.toggle("bi-x");
+    });
+});
+
 
 // Obtén el botón y añade un escuchador de eventos para cambiar los colores
 const colorButton = document.getElementById("color-button");
